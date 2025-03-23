e8bb18d7efde12b084ff8153c3ba8dde8398ae41
Johnsinoy <jcsinoy91@gmail.com>
2025-03-23 01:28:09 -0600

    Vehicle Insurance Management

---
+++ Vehicle-Insurance-Management/src/main/java/Java_Project/Vehicle_Insurance_Management/VehicleInsuranceManagementApplication.java
@@ -0,0 +1,13 @@
+package Java_Project.Vehicle_Insurance_Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleInsuranceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleInsuranceManagementApplication.class, args);
+	}
+
+}
---
+++ Vehicle-Insurance-Management/src/test/java/Java_Project/Vehicle_Insurance_Management/VehicleInsuranceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package Java_Project.Vehicle_Insurance_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleInsuranceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
